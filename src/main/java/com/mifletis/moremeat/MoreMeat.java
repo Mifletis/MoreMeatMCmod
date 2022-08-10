f8feccccffba7b7082e718f4e659033bb887b5bd
BuildTools <unconfigured@null.spigotmc.org>
2022-08-09 21:04:46 -0700

    Initial commit

---
+++ src/main/java/com/mifletis/moremeat/MoreMeat.java
@@ -0,0 +1,42 @@
+package com.mifletis.moremeat;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(MoreMeat.MODID_ID)
+public class MoreMeat
+{
+    public static final String MODID_ID = "moremeat";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MoreMeat()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
